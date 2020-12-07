ef9c0d180053f3ce23d3055eb4e6714ffd1c6d2b
MMilczynski <matthias.milczynsii@gmail.com>
2020-12-07 10:38:56 -0800

    Updated clean_project.bat

--- clean_project.bat
+++ clean_project.bat
@@ -1,13 +1,13 @@
 IF EXIST "__pycache__" (
-    RMDIR "__pycache__"
+    RMDIR "__pycache__" /Q /S
 )
 
 IF EXIST "build" (
-    RMDIR "build"
+    RMDIR "build" /Q /S
 ) 
 
 IF EXIST "dist" (
-    RMDIR "dist"
+    RMDIR "dist" /Q /S
 )
 
 IF EXIST "main.spec" (
@@ -15,14 +15,14 @@ IF EXIST "main.spec" (
 )
 
 IF EXIST "lib\__pycache__" (
-    RMDIR "lib\__pycache__"
+    RMDIR "lib\__pycache__" /Q /S
 )
 
 IF EXIST "tests\__pycache__" (
-    RMDIR "tests\__pycache__"
+    RMDIR "tests\__pycache__" /Q /S
 )
 
 IF EXIST ".pytest_cache" (
-    RMDIR ".pytest_cache"
+    RMDIR ".pytest_cache" /Q /S
 )
 
