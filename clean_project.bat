ed3723970001c7ca712d4b48dd91247e2be85627
MMilczynski <matthias.milczynsii@gmail.com>
2020-12-07 10:30:41 -0800

    Update build scripts

--- clean_project.bat
+++ clean_project.bat
@@ -1,5 +1,28 @@
-rm -r __pycache__ build dist 
-rm main.spec
-rm -r lib\__pycache__
-rm -r tests\__pycache__
-rm -r .pytest_cache
+IF EXIST "__pycache__" (
+    RMDIR "__pycache__"
+)
+
+IF EXIST "build" (
+    RMDIR "build"
+) 
+
+IF EXIST "dist" (
+    RMDIR "dist"
+)
+
+IF EXIST "main.spec" (
+    DEL "main.spec"
+)
+
+IF EXIST "lib\__pycache__" (
+    RMDIR "lib\__pycache__"
+)
+
+IF EXIST "tests\__pycache__" (
+    RMDIR "tests\__pycache__"
+)
+
+IF EXIST ".pytest_cache" (
+    RMDIR ".pytest_cache"
+)
+
